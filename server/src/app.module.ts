1e79ef82f928a88e91d815d8ca0a015f35352e6d
Maicol Hernandez <maykolandres4@gmail.com>
2024-12-27 05:07:10 -0500

    Fixed uri breaking issue

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SessionsModule } from './session/session.module';
 
+const mongoUri = process.env.MONGODB_URI?.replace(/"/g, '');
+
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), SessionsModule],
+  imports: [MongooseModule.forRoot(mongoUri), SessionsModule],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
